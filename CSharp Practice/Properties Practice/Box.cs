496a2b45fd35b026180c6cee9db6b1eda3c4f52d
Francisco Guzman <frankiejgiii@gmail.com>
2018-12-29 17:12:42 -0600

    Add files via upload

---
+++ CSharp Practice/Properties Practice/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesPractice
+{
+    class Box
+    {
+        private int length;
+        private int width;
+        private int height;
+
+        //create constructor
+        public Box(int length, int width, int height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+        //create properties
+        public int Length { get=>length; set=>length=value; }
+        public int Width { get=>width; set=>width=value; }
+        public int Height { get=>height; set=>height=value; }
+        public int Volume { get=>Length*Width*Height;}//read only
+        public int FrontSurface { get=>Length*Height;}//read only
+
+        //create method/s
+        public void DisplayInfo()
+        {
+            Console.WriteLine("The length is {0}, the width is {1}, and the height is {2}",length,width,height);
+        }
+    }
+}
---
+++ CSharp Practice/Properties Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(7,4,2);
+            box.DisplayInfo();
+            //change length,width,and height
+            box.Length = 2;
+            box.Width = 2;
+            box.Height = 3;
+            box.DisplayInfo();
+            Console.WriteLine("The volume of the box is:  {0}",box.Volume);
+            Console.WriteLine("The Front Surface of the box is:  {0}",box.FrontSurface);
+            Console.Read();
+        }
+    }
+}
